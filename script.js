7c8d5019347922fe364d94ffc0b70f6e3311d263
Liv <livbendix@live.dk>
2023-08-24 10:26:06 +0200

    ændret tekst og titel

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 const h1 = document.querySelector("h1");
-console.log(h1);
+console.log(h1.textContent);
+h1.innerHTML = "overskrift";
 
 const h2 = document.querySelector("h2");
-console.log(h2);
+console.log(h2.textContent);
+h2.innerHTML = "h2 tekst";
 
 const h3 = document.querySelector("h3");
-console.log(h3);
+console.log(h3.textContent);
+h3.innerHTML = "h3 tekst ændret";
 
 const p = document.querySelector("p");
-console.log(p);
+console.log(p.textContent);
+p.innerHTML = "p tekst ændret";
 
 const img = document.querySelector("img");
 console.log(img);
