6313c70636e40cb6d8c0dd052bd0c39cb8fda3e9
Liv <livbendix@live.dk>
2023-08-24 10:49:52 +0200

    ændret p tekst

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 const h1 = document.querySelector("h1");
 console.log(h1.textContent);
-h1.innerHTML = "overskrift";
+h1.textContent = "overskrift";
 
 const h2 = document.querySelector("h2");
 console.log(h2.textContent);
-h2.innerHTML = "h2 tekst";
+h2.textContent = "h2 tekst";
 
 const h3 = document.querySelector("h3");
 console.log(h3.textContent);
-h3.innerHTML = "h3 tekst ændret";
+h3.textContent = "h3 tekst ændret";
 
 const p = document.querySelector("p");
 console.log(p.textContent);
-p.innerHTML = "p tekst ændret";
+p.textContent = "p tekst ændret";
+
+const p2 = document.querySelector("article:nth-child(2) p");
+console.log(p.textContent);
+p2.innerHTML = "p tekst ændret";
 
 const img = document.querySelector("img");
 console.log(img);
